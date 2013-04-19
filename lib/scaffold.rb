d0ff7eec2d118a17534a283a76e8bfdadb47bcf4
Ohkubo KOHEI <ohkubo@magician.jp>
2013-04-19 21:02:30 +0900

    scaffold template

--- lib/scaffold.rb
+++ lib/scaffold.rb
@@ -1,22 +1,22 @@
-run 'git reset --hard'
-run 'git clean -f -d'
+#run 'git reset --hard'
+#run 'git clean -f -d'
 
-generate(:scaffold, "account name:uniq")
+generate(:scaffold, "user name:uniq email")
+generate(:model, "auth user:belongs_to provider uid")
 
-generate(:scaffold, "customer name mail")
-generate(:model, "email customer:belongs_to addr:uniq priority:integer")
+generate(:scaffold, "program uid:uniq url name description start_at end_at")
 
-generate :mailer, "CustomerMailer welcome seeyou blank"
+generate :mailer, "UserMailer programs"
 
-route "root :to => 'customers#index'"
+route "root :to => 'programs#index'"
 
 run 'rm app/views/layouts/application.html.erb'
 run 'rm app/assets/stylesheets/scaffolds.css.scss'
 
 rake("db:migrate:reset")
-run 'rm app/views/accounts/*'
-generate "bootstrap:themed accounts"
-run 'rm app/views/customers/*'
-generate "bootstrap:themed customers"
+run 'rm app/views/users/*'
+generate "bootstrap:themed users"
+run 'rm app/views/programs/*'
+generate "bootstrap:themed programs"
 
 print "rake rails:template LOCATION=lib/heroku.rb"
