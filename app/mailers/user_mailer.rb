6091284f90b89252ecdcc2bb75f7fdf3758f558d
Ohkubo KOHEI <ohkubo@magician.jp>
2013-04-21 02:44:59 +0900

    brush mail sending

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "tv@magician.jp"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -7,7 +8,7 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.programs.subject
   #
   def programs(user)
-    @programs = Program.where(status: :ok).where("? < start_at and start_at < ?", Time.now, 2.weeks.since).order("start_at ASC").group_by{|p| p.start_at.to_date}
-    mail to: user.email
+    @programs = Program.includes(:base_tags).where(status: :ok).where("? < start_at and start_at < ?", Time.now, 2.weeks.since).order("start_at ASC").group_by{|p| p.start_at.to_date}
+    mail to: user.email, subject: "[Magic TV] #{Date.today} から2週間のマジック番組"
   end
 end
--- lib/tasks/send_programs_mail.rake
+++ lib/tasks/send_programs_mail.rake
@@ -2,7 +2,7 @@ desc "sends mail to available emails"
 task :send_programs_mail => :environment do
   puts "Sending mail..."
   User.where("email IS NOT NULL").find_each do |user|
-    UserMailer.programs(current_user).deliver
+    UserMailer.programs(user).deliver
   end
   puts "done."
 end
