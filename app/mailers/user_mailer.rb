e42a3245347f2d33b1c65861ad3005059f9c8b03
Ohkubo KOHEI <ohkubo@magician.jp>
2013-04-24 03:47:15 +0900

    additional mail settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class UserMailer < ActionMailer::Base
-  default from: "tv@magician.jp"
+  default from: "noreply@magic-tv.trick-with.net"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ MagicTv::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => "http://magic-tv.trick-with.net" }
+  config.action_mailer.default_url_options = { :host => "magic-tv.trick-with.net" }
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
